21e32ba5703eb792101b06f82accb8dc3476a6b9
thanhlaunchdeck <thanh.le@launchdeck.org>
2024-01-13 20:29:16 +0700

    adjust router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthContext } from "../src/context/AuthContext"
+import { useContext } from "react";
 
 import MainLayout from "./components/MainLayout";
 
@@ -10,11 +12,12 @@ import LoginPage from "./pages/LoginPage";
 import Addaccount from "./pages/Addaccount";
 
 function App() {
+  const { token } = useContext(AuthContext);
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/sign-in" />} />
-        <Route path="/sign-in" element={<LoginPage />}></Route>
+        <Route path="/" element={token ? <Navigate to="admin" /> : <Navigate to="/sign-in" />} />
+        <Route path="/sign-in" element={token ? <Navigate to="/admin" /> : <LoginPage />}></Route>
         <Route path="/admin" element={<MainLayout />}>
           <Route index element={<Classes />} />
           <Route path="list-account" element={<Accountlist />} />
